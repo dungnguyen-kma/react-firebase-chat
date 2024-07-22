28a55141200e883d2ddbe83007e142998539b1e2
= <=>
2024-07-22 17:09:59 +0700

    init firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVkJMFwUE4i_13yAt_U8VUfhe_9tEqW8s",
+  authDomain: "reactchat-89164.firebaseapp.com",
+  projectId: "reactchat-89164",
+  storageBucket: "reactchat-89164.appspot.com",
+  messagingSenderId: "850720529669",
+  appId: "1:850720529669:web:d30edf49183286a207ab6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
